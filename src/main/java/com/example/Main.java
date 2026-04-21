374227044763102f0180cb75879067ad40248141
egoryanushev <zevs000023@gmail.com>
2026-04-21 11:02:10 +0300

    добавил ссылку на гитхаб проекта

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 /**
  * Главный класс приложения для демонстрации работы инжектора.
+ *
+ *  * @see <a href="https://github.com/METAMARGINAL/fifth_lab_java"_blank">Репозиторий проекта на GitHub</a>
+ *
  */
 public class Main {
     public static void main(String[] args) {
